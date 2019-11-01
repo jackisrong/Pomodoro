3c4789ebafb6dea5628652d67edd8f2c62005c46
jackisrong <34490671+jackisrong@users.noreply.github.com>
2019-11-01 15:38:19 -0700

    Added buttons and button CSS

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
 function countdown() {
-	
+    // 
+    var timeToAdd25MinutesInMilliseconds = 25 * 60 * 1000;
+    //var countDownDate = new Date("Jan 5, 2021 15:37:25").getTime();
+    var countdownDate = new Date().getTime() + timeToAdd25MinutesInMilliseconds;
+
+    // Update the count down every 1 second
+    var x = setInterval(function () {
+
+        // Get today's date and time
+        var now = new Date().getTime();
+
+        // Find the distance between now and the count down date
+        var distance = countdownDate - now;
+
+        // Time calculations for days, hours, minutes and seconds
+        //var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        //var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        // Display the result in the element with id="demo"
+        document.getElementById("countdown").innerHTML = minutes + "m " + seconds + "s ";
+
+        // If the count down is finished, write some text
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("countdown").innerHTML = "EXPIRED";
+        }
+    }, 1000);
 }
