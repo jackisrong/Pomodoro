6641b526448e89f6b42825a2cc4f0d2bd644e404
jackisrong <34490671+jackisrong@users.noreply.github.com>
2019-11-01 16:08:24 -0700

    Fixed internal code merge issues

--- script.js
+++ script.js
@@ -1,27 +1,23 @@
 function startTimer() {
+    var initialTime = Date.now();
+    
+    var tick = setInterval(function countdown() {
+        var timePassed = 25 * 60 - ((Date.now() - initialTime) / 1000 | 0);
 
-	var initialTime = Date.now();
-
-	var tick = setInterval(function countdown() {
-		var timePassed = 25 * 60 + 1 - ((Date.now() - initialTime) / 1000 | 0);
-		
-		var minutes = (timePassed / 60) | 0;
+        var minutes = (timePassed / 60) | 0;
         var seconds = (timePassed % 60) | 0;
 
         minutes = minutes < 10 ? "0" + minutes : minutes;
         seconds = seconds < 10 ? "0" + seconds : seconds;
 
-        document.getElementById("time").innerHTML = minutes + ":" + seconds;
+        document.getElementById("countdown").innerHTML = minutes + ":" + seconds;
 
         if (timePassed == 0) {
-        	clearInterval(tick);
-        	console.log("End of time!");
+            clearInterval(tick);
+            console.log("End of time!");
         }
- 
-	}, 1000);
+
+    }, 1000);
 }
 
-window.onload = function () {
-    startTimer();
-}
 
