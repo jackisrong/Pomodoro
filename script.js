c4b8641910f31fb923a61cbc00eb1ad06fb204ca
Catherine Li <catherine.li34@gmail.com>
2019-11-01 15:50:36 -0700

    More efficient timer

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
-function countdown() {
-	
+function startTimer() {
+
+	var initialTime = Date.now();
+
+	var tick = setInterval(function countdown() {
+		var timePassed = 25 * 60 + 1 - ((Date.now() - initialTime) / 1000 | 0);
+		
+		var minutes = (timePassed / 60) | 0;
+        var seconds = (timePassed % 60) | 0;
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        document.getElementById("time").innerHTML = minutes + ":" + seconds;
+
+        if (timePassed == 0) {
+        	clearInterval(tick);
+        	console.log("End of time!");
+        }
+ 
+	}, 1000);
+}
+
+window.onload = function () {
+    startTimer();
 }
